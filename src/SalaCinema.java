9679cbde34d55becd6a285ecd1226386f02b1f75
dibenedetto.ivan <dibenedettoivan@gmail.com>
2015-05-22 22:34:33 +0200

    Modifica progetto Gestione Cinema

--- src/SalaCinema.java
+++ src/SalaCinema.java
@@ -1,4 +1,33 @@
 
+
+
 public class SalaCinema {
+	
+	private String film = "";
+	private int postiOccupati;
+	private int numeroFile;
+	final private int maxPerFila;
+	private double incasso;
+	
+	public SalaCinema(String film, int numFile){
+		film ="The Avengers";
+		numFile = numeroFile;
+		postiOccupati = 0;	
+		incasso = 0;
+		maxPerFila = 24;
+		
+	}
+	public int getPostiLiberi( int numFile, int maxPerFila){
+		return numFile * maxPerFila - postiOccupati;
+		
+		
+	}
+	public void venditaBiglietti(int postiDaOccupare, String tipo){
+		int postiDaOccupare = postiLiberi - postiOccupati;
+		
+		
+	}
+	
+	
 
 }
